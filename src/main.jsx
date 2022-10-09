060e1ad1165e4f7bf27540d66e0c225344ac53d5
My Name <mdarfinji45@gmail.com>
2022-10-09 10:12:21 +0800

    adding

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import ErrorPage from "./error-page";
 import  { loader as rootLoader, action as rootAction, } from "./routes/root";
 import Contact,{ loader as contactLoader,} from "./routes/contact";
 import './index.css'
-
+import EditContact , {
+  action as editAction,
+}  from "./routes/edit";
 const router =createBrowserRouter([
   {
     path: "/",
@@ -20,6 +22,12 @@ const router =createBrowserRouter([
         path: "contacts/:contactId",
         element: <Contact />,
         loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
       },
     ],
   },
